c82a10ea45e31b96af8cc33154cf56d5e2431a9c
Dario Valdez <darteto@gmail.com>
2026-05-28 17:15:59 -0500

    Edit main.py para ejecutar cron en server

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
 from app.scripts.run_scrapers_altenar import ejecutar_scrapers_altenar
 from app.scripts.run_scrapers_sportradar import ejecutar_scrapers_sportradar
 from app.services.limpieza_service import limpiar_cuotas_antiguas
 
-def ejecutar_scraping():
+def main():
     print("===================================")
     print("INICIANDO SCRAPING")
     print("===================================")
 
     try:
         ejecutar_scrapers_altenar()
-        #ejecutar_scrapers_sportradar()
+        ejecutar_scrapers_sportradar()
+        limpiar_cuotas_antiguas()
 
     except Exception as e:
         print("Error general:", e)
@@ -21,22 +21,4 @@ def ejecutar_scraping():
 
 
 if __name__ == "__main__":
-    scheduler = BlockingScheduler()
-
-    scheduler.add_job(
-        ejecutar_scraping,
-        "interval",
-        hours=1
-    )
-
-    scheduler.add_job(
-        limpiar_cuotas_antiguas,
-        "cron",
-        hour=3
-    )
-
-    ejecutar_scraping()
-
-    print("Scheduler iniciado. Scraping cada 1 hora...")
-
-    scheduler.start()
+    main()
